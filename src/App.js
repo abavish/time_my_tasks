d3f376fae78ce144dd26b3ef61135ccf5197acc1
Abavish <vnand@csu.fullerton.edu>
2018-09-23 18:45:41 -0700

    wiped off default template and added semantic ui

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App main ui">
+           <h1 className="ui dividing centered header">Timers</h1>
+           
+      </div>
+    );
+  }
+}
+
+export default App;
